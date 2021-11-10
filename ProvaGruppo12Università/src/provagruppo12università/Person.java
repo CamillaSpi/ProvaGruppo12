890cc07cfeab65dd18a6b985310d0ee4b4a954c7
alcatraz <ferdisica@icloud.com>
2021-11-10 18:26:11 +0100

    Tested Person Class

---
+++ ProvaGruppo12Università/src/provagruppo12università/Person.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package provagruppo12università;
+
+/**
+ *
+ * @author nando
+ * @time Nov 10, 2021 6:20:04 PM
+ */
+public class Person {
+    private String name;
+    private String surname;
+    private int eta;
+    private int sesso;
+
+    public Person(String name, String surname, int eta , int sesso) {
+        this.name = name;
+        this.surname = surname;
+        this.sesso = sesso;
+        this.eta = eta;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public int getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(int sesso) {
+        this.sesso = sesso;
+    }
+
+    @Override
+    public String toString() {
+        return "Il nome del soggetto e'" + " name=  " + name + ", surname = " + surname + ", eta = " + eta + " sesso = Decisamente pcoo ";
+    }
+    
+    
+            
+    
+    
+
+}
