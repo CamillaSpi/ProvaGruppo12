e39f7e625ae44808c7d1e4e6cafc6b7f4cc0174d
alcatraz <ferdisica@icloud.com>
2021-11-10 17:29:26 +0100

    New Release with more hi

--- ProvaGruppo12Università/src/provagruppo12università/ProvaGruppo12Università.java
+++ ProvaGruppo12Università/src/provagruppo12università/ProvaGruppo12Università.java
@@ -17,6 +17,7 @@ public class ProvaGruppo12Università {
     public static void main(String[] args) {
         System.out.println("Ciao ragazzi");
         System.out.println("Ciao ragazzi da parte di nando ");
+        System.out.println("Ciao ragazzi da parte di Mattia Marseglia che fa gli esercizi ");
     }
     
 }
