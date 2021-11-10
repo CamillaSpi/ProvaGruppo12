d5fdd2e94bde9e4ae68cf8942ee921a3eb4b39f7
t440 <t440@DESKTOP-VSHOVOS.mshome.net>
2021-11-10 18:13:52 +0100

    Merge origin/main into main
    Conflicts:
    	ProvaGruppo12Università/src/provagruppo12università/ProvaGruppo12Università.java

--- ProvaGruppo12Università/src/provagruppo12università/ProvaGruppo12Università.java
+++ ProvaGruppo12Università/src/provagruppo12università/ProvaGruppo12Università.java
@@ -23,6 +23,14 @@ public class ProvaGruppo12Università {
         return a-b;
     }
     
+    public static int SommaDiPiuValori (int[] vect){
+        int sum = 0;
+        for (int x: vect)
+            sum += x;
+        System.out.println("Ciao Ragazzi la somma del vettore sarà: " +sum);
+        return sum;
+    }
+    
     
     public static String FunzionediVito(String x){
         String result;
@@ -40,6 +48,8 @@ public class ProvaGruppo12Università {
         String x = "Nando";
         System.out.println(FunzionediVito(x));
         SommaDiDueValori(5,3);
+        int[] v={1,2,3,4};
+        SommaDiPiuValori(v);
         SottrazioneaDiDueValori(5,3);
         System.out.println("cazzoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
     }
